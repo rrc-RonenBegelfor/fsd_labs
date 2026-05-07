c5f810c324c023ee35301c2395efb91733f733c9
Ronen Begelfor <rbegelfor@rrc.ca>
2026-05-07 17:54:55 -0500

    Feat: Developed working script. Added data.json that holds all Pixell Department and employee information. Added some css.

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
-let footer = document.getElementById("footer");
-const currentYear = new Date().getFullYear();
-footer.innerText += ` ${currentYear}`;
+const footerFunction = () => {
+    let footer = document.getElementById("footer");
+    const currentYear = new Date().getFullYear();
+    footer.innerText += ` ${currentYear}`;
+}
+
+const employeeGeneration = () => {
+    fetch("./assets/data.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    
+        const employeeInformationDiv = document.getElementById('employeeInformation')
+
+    
+        Object.entries(data).forEach(([department, employees]) => {
+            const section = document.createElement('section');
+            section.classList.add("department");
+
+            const headingTwo = document.createElement('h2');
+            headingTwo.textContent = department;
+
+            section.appendChild(headingTwo);
+
+            const list = document.createElement('ul');
+
+            employees.forEach(employee => {
+                const bulletPoint = document.createElement('li');
+                bulletPoint.textContent = employee;
+                
+                list.appendChild(bulletPoint);
+            });
+
+            section.appendChild(list);
+            employeeInformationDiv.appendChild(section);
+        })
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    footerFunction();
+    employeeGeneration();
+});
