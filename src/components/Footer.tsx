60e7c8917a1489156fc1621ba76e2c5241dcacbc
Ronen Begelfor <rbegelfor@rrc.ca>
2026-05-22 08:49:24 -0500

    Feat: added footer.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+
+    function footerYear() {
+        const currentYear = new Date().getFullYear()
+
+        return currentYear;
+    }
+
+
+    return <>
+        <footer id="footer">Copyright Pixell River {footerYear()}</footer>
+    </>
+}
