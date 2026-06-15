c7fbc0dcca7af76d89dafe4410c6e3ad5d800273
Ronen Begelfor <rbegelfor@rrc.ca>
2026-06-15 15:15:12 -0500

    Feat: Import changes. LeadershipForm tsx and css. Reformatting of Employee and Organization pages.

--- src/components/Pages/OrganizationPage.tsx
+++ src/components/Pages/OrganizationPage.tsx
@@ -1,11 +1,20 @@
 import { useLeaders } from "../../hooks/useLeaders"
 import LeadershipRoleList from "../LeadershipRoleList/LeadershipRoleList"
+import LeadershipForm from "../LeadershipForm/LeadershipForm"
 
 export default function OrganizationPage() {
 
-    const { leaders, loading, error} = useLeaders();
+    const { leaders, loading, error, addLeader} = useLeaders();
 
     return <>
-        {error ? (<div>{error}</div>) :loading ? (<div className="blink">Loading leaders...</div>) : (<LeadershipRoleList leaders={leaders}/>)}
+        {error ? (<div>{error}</div>
+        ) : loading ? (<div className="blink">Loading leaders...</div>
+
+        ) : (
+            <>
+                <LeadershipRoleList leaders={leaders}/>
+                <LeadershipForm addLeader={addLeader} leaders={leaders}/>
+            </>
+        )}
     </>
 }
